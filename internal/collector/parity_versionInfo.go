bfbc0bdcccc4c7153ed32fdade852af3e318ccf4
Oliver Deutsch <odeutsch@forte.io>
2021-03-23 19:05:30 +0100

    test

--- internal/collector/parity_versionInfo.go
+++ internal/collector/parity_versionInfo.go
@@ -1,9 +1,7 @@
 package collector
 
 import (
-	"encoding/json"
 	"log"
-	"strconv"
 	"time"
 
 	"github.com/ethereum/go-ethereum/rpc"
@@ -15,23 +13,13 @@ type ParityVersionInfo struct {
 	desc *prometheus.Desc
 }
 
-type version struct {
-	Major int
-	Minor int
-	Patch int
-}
-
-type versionResult struct {
-	Version version
-}
-
 func NewParityVersionInfo(rpc *rpc.Client) *ParityVersionInfo {
 	return &ParityVersionInfo{
 		rpc: rpc,
 		desc: prometheus.NewDesc(
 			"parity_versionInfo",
 			"Provides information about running version of Parity version",
-			[]string{"version"},
+			nil,
 			nil,
 		),
 	}
@@ -42,23 +30,23 @@ func (collector *ParityVersionInfo) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (collector *ParityVersionInfo) Collect(ch chan<- prometheus.Metric) {
-	var raw json.RawMessage
+	var result *[]interface{}
 	start := time.Now()
-	if err := collector.rpc.Call(&raw, "parity_versionInfo"); err != nil {
+	if err := collector.rpc.Call(&result, "web3_clientVersion"); err != nil {
 		errorEnd := time.Now()
-		log.Print("error parityVersionInfo: ", errorEnd.Sub(start))
+		log.Print("error web3_clientVersion: ", errorEnd.Sub(start))
 		ch <- prometheus.NewInvalidMetric(collector.desc, err)
 		return
 	}
 	end := time.Now()
 
-	log.Print("parity_versionInfo: ", end.Sub(start))
-	var result *versionResult
-	if err := json.Unmarshal(raw, &result); err != nil {
-		ch <- prometheus.NewInvalidMetric(collector.desc, err)
-		return
-	}
+	log.Print("web3_clientVersion: ", end.Sub(start))
+	log.Print(*result)
+	// if err := json.Unmarshal(raw, &result); err != nil {
+	// 	ch <- prometheus.NewInvalidMetric(collector.desc, err)
+	// 	return
+	// }
 	value := float64(1)
-	versionValue := strconv.Itoa(result.Version.Major) + "." + strconv.Itoa(result.Version.Minor) + "." + strconv.Itoa(result.Version.Patch)
-	ch <- prometheus.MustNewConstMetric(collector.desc, prometheus.GaugeValue, value, versionValue)
+	// versionValue := strconv.Itoa(result.Version.Major) + "." + strconv.Itoa(result.Version.Minor) + "." + strconv.Itoa(result.Version.Patch)
+	ch <- prometheus.MustNewConstMetric(collector.desc, prometheus.GaugeValue, value)
 }
